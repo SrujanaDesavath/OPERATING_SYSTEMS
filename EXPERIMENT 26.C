6d81ed98000ffaeacc41bb3db5699f517bfa7943
SrujanaDesavath <192571067.simats@saveetha.com>
2026-06-02 11:17:10 +0530

    EXPERIMENT 26.C

---
+++ EXPERIMENT 26.C
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int blockSize[20], processSize[20];
+    int m, n, i, j, worstIdx, allocation[20];
+
+    printf("Enter number of blocks: ");
+    scanf("%d",&m);
+
+    printf("Enter block sizes:\n");
+    for(i=0;i<m;i++) scanf("%d",&blockSize[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    printf("Enter process sizes:\n");
+    for(i=0;i<n;i++) scanf("%d",&processSize[i]);
+
+    for(i=0;i<n;i++) allocation[i] = -1;
+
+    for(i=0;i<n;i++) {
+        worstIdx = -1;
+        for(j=0;j<m;j++) {
+            if(blockSize[j] >= processSize[i]) {
+                if(worstIdx == -1 || blockSize[j] > blockSize[worstIdx])
+                    worstIdx = j;
+            }
+        }
+
+        if(worstIdx != -1) {
+            allocation[i] = worstIdx;
+            blockSize[worstIdx] -= processSize[i];
+        }
+    }
+
+    printf("\nProcess\tSize\tBlock\n");
+    for(i=0;i<n;i++) {
+        printf("%d\t%d\t", i+1, processSize[i]);
+        if(allocation[i]!=-1) printf("%d\n", allocation[i]+1);
+        else printf("Not Allocated\n");
+    }
+}
