676db0343301edb5daf390777b72a923ba7ab716
SrujanaDesavath <192571067.simats@saveetha.com>
2026-06-02 11:21:59 +0530

    EXPERIMENT 30.C

---
+++ EXPERIMENT 30.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* fun(void* arg) {
+    printf("Thread executing\n");
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, fun, NULL);
+    pthread_create(&t2, NULL, fun, NULL);
+
+    if (pthread_equal(t1, t2))
+        printf("Equal threads\n");
+    else
+        printf("Not equal threads\n");
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Threads completed\n");
+}
