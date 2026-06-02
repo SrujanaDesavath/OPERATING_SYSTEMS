417ed96cce454527c755f8798b1d26c4e30e03be
SrujanaDesavath <192571067.simats@saveetha.com>
2026-06-02 11:18:03 +0530

    EXPERIMENT 27.C

---
+++ EXPERIMENT 27.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main() {
+    DIR *dir;
+    struct dirent *entry;
+    char path[100];
+
+    printf("Enter directory path: ");
+    scanf("%s", path);
+
+    dir = opendir(path);
+
+    if (dir == NULL) {
+        printf("Cannot open directory\n");
+        return 0;
+    }
+
+    printf("\nFiles:\n");
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+}
