a0517547c8bdf7665e49523dd28080ceccc738a7
SrujanaDesavath <192571067.simats@saveetha.com>
2026-06-02 11:27:32 +0530

    EXPERIMENT  36.C

---
+++ EXPERIMENT  36.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int blocks[20], n, i;
+
+    printf("Enter number of blocks: ");
+    scanf("%d", &n);
+
+    printf("Enter block numbers:\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&blocks[i]);
+
+    printf("Linked Allocation:\n");
+
+    for(i=0;i<n-1;i++)
+        printf("%d -> ", blocks[i]);
+
+    printf("%d -> NULL", blocks[n-1]);
+}
